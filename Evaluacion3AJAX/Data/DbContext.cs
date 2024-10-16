e52a2b86bf51263659ebb12b868731669de04048
Alumno <Alumno@DESKTOP-9QM0RTU>
2024-10-16 15:08:26 -0300

    Agregar archivos de proyecto.

---
+++ Evaluacion3AJAX/Data/DbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Evaluacion3AJAX.Models;
+namespace Evaluacion3AJAX.Data
+{
+    public class ProyectoDbContext : DbContext
+    {
+        public ProyectoDbContext(DbContextOptions<ProyectoDbContext> options) : base(options)
+        {
+        }
+
+        /* DbSet indica el modelo que se va a mapear (reflejar) a la base de datos */
+        public DbSet<Rol> Roles { get; set; }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DbSet<Tarea> Tareas { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Acá se pueden cargar los datos iniciales de la base de datos
+
+            modelBuilder.Entity<Rol>().HasData(new Rol
+            {
+                Id = 1,
+                Nombre = "Administrador"
+            });
+
+            modelBuilder.Entity<Rol>().HasData(new Rol
+            {
+                Id = 2,
+                Nombre = "Empleado"
+            });
+
+        }
+
+    }
+
+}
---
+++ Evaluacion3AJAX/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace Evaluacion3AJAX.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        // Relación uno a muchos entre Usuario y Rol, o sea, un usuario puede tener un rol
+
+        [ForeignKey("Rol")]
+        public int RolId { get; set; }
+    }
+}
